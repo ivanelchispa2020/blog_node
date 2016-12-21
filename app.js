740b6f4b2d0549f9d48757a540b9c662029b1ccc
ivan <ivanlopez_88@hotmail.com>
2016-12-21 19:02:53 -0300

    prrll

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(formidable.parse({
 				encoding: 'utf-8',
-  				uploadDir: 'avatares',
+  				uploadDir: 'public/images/avatarres',
 				keepExtensions: true //mantenga la extension
 }));
 
