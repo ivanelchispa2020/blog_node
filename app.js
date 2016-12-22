01aa1b105065d1580e0690041724d622b06aa8c7
ivan <ivanlopez_88@hotmail.com>
2016-12-22 11:15:57 -0300

    error iamgen 52

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(formidable.parse({
-		uploadDir: 'C:/Users/ivan/Downloads/images_node',
+		uploadDir: '/images/avatares',
 		keepExtensions: true //mantenga la extension
 }));
 
