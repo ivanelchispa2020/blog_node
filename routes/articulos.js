0fe405624cccf2152c93388529670d34c49d7c58
ivan <ivanlopez_88@hotmail.com>
2016-12-22 10:18:23 -0300

    recuperando images

--- routes/articulos.js
+++ routes/articulos.js
@@ -236,10 +236,9 @@ var ultimos_articulos={};
 
 					comentario.save().then((comen)=>{
 
-						mv(req.body.file_imagen.path, 'public/images/avatares/'+comen.id +"."+ extension,function(err) {
-						    if (err) { console.log(err) }
-						console.log('file moved successfully');
-};						)
+	mv(req.body.file_imagen.path, 'public/images/avatares/'+comen.id +"."+ extension,function(err) {
+							console.log(err);
+						})
 
 									},(err)=>{
 									 console.log("Ha ocurrido un error ");
